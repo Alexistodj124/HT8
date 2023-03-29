f07f358983d4ff89e2fa041ea3a096405bec2073
Alexistodj124 <alexistodj@gmail.com>
2023-03-29 09:57:03 -0600

    creacion de vectorHeap y su interfaz priorityQueue

--- priorityQueue.java
+++ priorityQueue.java
@@ -1,3 +1,8 @@
-public class priorityQueue {
-    
-}
+public interface priorityQueue<E extends Comparable<E>> {
+    public E getFirst();
+    public boolean isEmpty();
+    public int size();
+    public void clear();
+    public void add(E value);
+    public E remove();
+  }
