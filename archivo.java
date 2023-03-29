0e4d4c71fbba1d854ee2b20f7306c381de33e66c
Alexistodj124 <alexistodj@gmail.com>
2023-03-28 23:30:23 -0600

    Creacion de clase Archivo y Clase proceso

---
+++ archivo.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+public class archivo {
+    private ArrayList<proceso> lista;
+
+    public archivo(String nombre) {
+        this.lista = new ArrayList<proceso>();
+    }
+
+    public static int convert(String numeroStr) {
+        int numeroInt;
+        if (numeroStr == null){
+            numeroInt = 0;
+        }else {
+            numeroInt = Integer.parseInt(numeroStr);
+        }
+        return numeroInt;
+    }
+
+    public ArrayList<proceso> leerArchivo() throws IOException {
+        File archivo = new File("procesos.txt");
+        FileReader lector = new FileReader(archivo);
+        try (BufferedReader lectorBuffer = new BufferedReader(lector)) {
+            String linea;
+            while ((linea = lectorBuffer.readLine()) != null) {
+                String[] lista = linea.split(",");
+                if (lista.length != 3) {
+                    throw new IllegalArgumentException("Esta mal escrita la linea: " + linea);
+                }
+                String nombreProceso = lista[0];
+                String nombreUsuario = lista[1];
+                int nice = convert(lista[2]);
+                int prioridad = 0;
+
+                proceso process = new proceso(nombreProceso, nombreUsuario, nice, prioridad);
+                this.lista.add(process);
+
+            }
+
+        }catch(Exception e){System.out.println(e);}
+        return this.lista;
+
+    }
+}
--- proceso.java
+++ proceso.java
@@ -1,3 +1,40 @@
 public class proceso {
-    
+    private String Proceso;
+    private String Usuario;
+    private int Nice;
+    private int PR;
+    public proceso(String proceso, String usuario, int nice, int pR) {
+        Proceso = proceso;
+        Usuario = usuario;
+        Nice = nice;
+        PR = pR;
+    }
+    public String getProceso() {
+        return Proceso;
+    }
+    public void setProceso(String proceso) {
+        Proceso = proceso;
+    }
+    public String getUsuario() {
+        return Usuario;
+    }
+    public void setUsuario(String usuario) {
+        Usuario = usuario;
+    }
+    public int getNice() {
+        return Nice;
+    }
+    public void setNice(int nice) {
+        Nice = nice;
+    }
+    public int getPR() {
+        return PR;
+    }
+    public void setPR(int pR) {
+        PR = pR;
+    }
+    public String toString() {
+        int pr = 20 + Nice;
+        return Proceso + "," + Usuario + "," + Nice + ",PR=" + pr;
+    }
 }
